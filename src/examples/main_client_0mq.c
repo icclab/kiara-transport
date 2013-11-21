216346568e931e70cccc0cd9c829b8598c381972
Mathias Hablützel <habl@zhaw.ch>
2013-11-21 15:27:55 +0100

    Simple example for ZeroMQ Request/Reply

--- src/examples/main_client_0mq.c
+++ src/examples/main_client_0mq.c
@@ -1,42 +1,17 @@
-/* 
- * File:   ktransport.h
- * Author: aepp
- *
- * Created on 26. Juli 2013, 13:58
- */
-//Example of KIARAClient/Server API
-#include "../../src/core/kt_client.h"
+#include "../../include/k_transport.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 int main()
 {
-	kt_connconf_t config;
-	char *msgData = "Hello server? You there?\0";
+  kt_conn_session_t* sess = kt_connect ("tcp://160.85.122.107:5555");
+  
+  kt_msg_t* msg = kt_msg_new();
+  char* payload = "Blabla";
+  kt_msg_set_payload (msg, payload, 6, NULL);
 
-	//set the config
-	config.network_config.network = IPLEGACY;
-	config.network_config.transport = TCP;
-	config.network_config.crypto = NONE;
-	config.network_config.application = ZEROMQ;
-	config.network_config.port = 5670;
-	config.type = REQUESTREPLY;
-	config.base_url = "localhost";
-
-	//initialize the client (network nego phase)
-	kt_clientctx_t *k_ctx = kt_client_init(config);
-	//connect to the endpoint
-	kt_client_connect(k_ctx);
-
-	//simulate request
-	kt_messageraw_t *msg = malloc(sizeof(kt_messageraw_t));
-	msg->msgData = msgData;
-	send_message(k_ctx, msg);
-
-	//receive reply, dump it
-	msg = recv_message(k_ctx);
-	//printf("%s\n", msg->msgData);
-
-	kt_client_disconnect(k_ctx);
-
+  kt_send ( sess, msg, 0);
+  msg = kt_recv ( sess, 0 );
+  printf ("%s\n", (char*) kt_msg_get_payload(msg));
 	return 0;
 }
