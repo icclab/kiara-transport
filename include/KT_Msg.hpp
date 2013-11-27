00389cdde80976d22a4ba22dcb57a97dda72faaa
Mathias Hablützel <habl@zhaw.ch>
2013-11-27 16:53:53 +0100

    KT_Msg class

---
+++ include/KT_Msg.hpp
@@ -0,0 +1,84 @@
+/**
+ * KIARA Transport Library Message
+ * 
+ * Author: Mathias Hablützel <habl@zhaw.ch>
+ */
+
+#ifndef KT_MSG_HPP
+#define KT_MSG_HPP
+
+#include <string>
+#include <map>
+#include <vector>
+
+namespace KIARA {
+namespace Transport {
+
+
+/**
+  * class KT_Msg
+  * Since payload is handled as a pointer to a binary memory allocation
+  * *free_payload() is responsible to destroy/deallocate the beforementioned
+  * memory.
+  * 
+  * According to settings in kt_conn_session_t defined it will generate valid
+  * header and body structures from the metadata and payload when sent. The
+  * same applies the other way around when receiving messages.
+  */
+
+class KT_Msg
+{
+private:
+  std::map< std::string, std::string > _metadata;
+  std::vector< unsigned char > _payload;
+
+public:
+  KT_Msg ( );
+  virtual ~KT_Msg ( );
+  KT_Msg ( std::vector< unsigned char >& payload);
+
+  void add_metadata ( std::string &key, std::string &value );
+  std::string get_serialized_metadata ( const std::string delim );
+  
+  //
+  // Simple getter/setter methods
+  //
+
+  /**
+   * Set the value of _metadata
+   * @param metadata the new value of _metadata
+   */
+  void set_metadata ( std::map< std::string, std::string > &metadata ) {
+      _metadata = metadata;
+  }
+
+  /**
+   * Get the value of _metadata
+   * @return the value of _metadata
+   */
+  std::map< std :: string, std :: string > get_metadata ( ) {
+    return _metadata;
+  }
+
+  /**
+   * Set the value of _payload
+   * @param payload the new value of _payload
+   */
+  void set_payload ( std::vector< unsigned char > payload ) {
+    _payload = payload;
+  }
+
+  /**
+   * Get the value of _payload
+   * @return the value of _payload
+   */
+  std::vector< unsigned char > get_payload ( ) {
+    return _payload;
+  }
+
+
+};
+} // end of Transport namespace
+} // end of KIARA namespace
+
+#endif // KT_MSG_HPP
---
+++ src/core/KT_Msg.cpp
@@ -0,0 +1,56 @@
+/**
+ * KIARA Transport Library Message
+ * 
+ * Author: Mathias Hablützel <habl@zhaw.ch>
+ */
+
+#include "../../include/KT_Msg.hpp"
+
+namespace KIARA {
+namespace Transport {
+
+//
+// Constructors/Destructors
+//  
+
+KT_Msg::KT_Msg ( ) { }
+KT_Msg::~KT_Msg ( ) { }
+
+KT_Msg::KT_Msg ( std::vector< unsigned char > &payload) {
+    _payload = payload;
+}
+
+//  
+// Methods
+//
+
+/**
+ * Add a key-value metadata pair to _metadata
+ * @param key A std::string as key
+ * @param value A std::string as value
+ */
+void KT_Msg::add_metadata ( std::string &key, std::string &value ) {
+    std::pair < std::string, std::string > elem (key, value);
+    _metadata.insert ( elem );
+}
+
+/**
+ * Return a serialized variant of the _metadata
+ * @param delim A delimiter, standard is ':'
+ * @return A std::string with a serialized variant
+ */
+std::string KT_Msg::get_serialized_metadata ( const std::string delim = ":" ) {
+    std::map < std::string, std::string >::iterator metadata_iter;
+    std::string result;
+    
+    for ( metadata_iter = _metadata.begin();
+            _metadata.end() != metadata_iter;
+            metadata_iter++ )
+    {
+        result += metadata_iter->first + delim + metadata_iter->second + "\n";
+    }
+    return result;
+}
+
+} // end of Transport namespace
+} // end of KIARA namespace
