b52f496339735ed74a8057d4bf9f36927c53c277
Mathias Hablützel <habl@zhaw.ch>
2013-12-02 16:27:30 +0100

    Finished KT_Zeromq::disconnect and fixed namespace problems

--- src/core/KT_Zeromq.cpp
+++ src/core/KT_Zeromq.cpp
@@ -11,11 +11,17 @@
 #include <cstdlib> // for malloc
 #include <zmq.h>
 
+KIARA::Transport::KT_Zeromq::KT_Zeromq() {
+	_context = zmq_ctx_new ();
+}
+
+KIARA::Transport::KT_Zeromq::~KT_Zeromq() {
+	zmq_ctx_destroy ( _context );
+}
+
 KIARA::Transport::KT_Session*
 KIARA::Transport::KT_Zeromq::connect ( KIARA::Transport::KT_Client &endpoint ) {
-	_context = zmq_ctx_new ();
 	void* socket = zmq_socket ( _context, ZMQ_REQ );
-
 	const char* host = endpoint.get_endpoint().c_str();
 	int rc = zmq_connect ( socket, host );
 
@@ -24,6 +30,7 @@ KIARA::Transport::KT_Zeromq::connect ( KIARA::Transport::KT_Client &endpoint ) {
 
 	KIARA::Transport::KT_Session *session = new KIARA::Transport::KT_Session ();
 	session->set_socket ( socket );
+	session->set_endpoint ( endpoint.get_endpoint() );
 
 	std::string *identifier = new std::string (endpoint.get_endpoint());
 	_sessions.insert ( std::pair<std::string*, KIARA::Transport::KT_Session*>(identifier, session) );
@@ -32,7 +39,7 @@ KIARA::Transport::KT_Zeromq::connect ( KIARA::Transport::KT_Client &endpoint ) {
 }
 
 void
-send ( KIARA::Transport::KT_Msg &message, KIARA::Transport::KT_Session &session, int linger = 0 ) {
+KIARA::Transport::KT_Zeromq::send ( KIARA::Transport::KT_Msg &message, KIARA::Transport::KT_Session &session, int linger) {
 	void* payload = malloc ( message.get_payload()->size() + 1 );
 	memcpy ( payload, message.get_payload()->data(), message.get_payload()->size() );
 	zmq_send ( session.get_socket(), payload, message.get_payload()->size() + 1, linger );
@@ -40,7 +47,7 @@ send ( KIARA::Transport::KT_Msg &message, KIARA::Transport::KT_Session &session,
 }
 
 KIARA::Transport::KT_Msg*
-recv ( KIARA::Transport::KT_Session& session, int linger = 0 ) {
+KIARA::Transport::KT_Zeromq::recv ( KIARA::Transport::KT_Session& session, int linger ) {
 	// TODO: Remove magic number
 	std::vector< unsigned char > *buffer = new std::vector< unsigned char >(1024);
 	int rc = zmq_recv ( session.get_socket(), &buffer[0], 1024, linger);
@@ -53,60 +60,23 @@ recv ( KIARA::Transport::KT_Session& session, int linger = 0 ) {
 	return message;
 }
 
+void
+KIARA::Transport::KT_Zeromq::disconnect ( KIARA::Transport::KT_Session& session ) {
+	zmq_close ( session.get_socket() );
+	std::string *key = new std::string ( session.get_endpoint() );
+
+	// something bugs me … does it then compare the address of the key or the value?
+	// since this is a pointer it might not get dereferenced and the address used instead
+	// the actual value rendering the erase useless or even dangerous.
+	_sessions.erase( key );
+}
 /*
-
-kt_msg* kt_msg_new ()
-{
-  return reinterpret_cast<kt_msg*>(new KIARA::Transport::KT_Msg());
-}
-
-void kt_msg_destroy ( kt_msg* msg )
-{
-  // Prevent working on NULL
-  assert (msg);
-  delete (msg);
-}
-
-void kt_msg_set_payload ( kt_msg* c_msg, void* payload, size_t payload_size)
-{
-  KIARA::Transport::KT_Msg *msg = reinterpret_cast<KIARA::Transport::KT_Msg *> (c_msg);
-}
-
-void* kt_msg_get_payload ( kt_msg* c_msg )
-{
-  KIARA::Transport::KT_Msg *msg = reinterpret_cast<KIARA::Transport::KT_Msg *> (c_msg);
-  return msg->get_payload().data ();
-}
-
-kt_application_layer _return_transport_from_endpoint (const char* rem)
-{
-  assert (rem != NULL);
-  std::string remote(rem);
-
-  if ( remote.compare (0, 3, "tcp") == 0 )
-    return KT_TCP;
-
-  return 0;
-}
 
 void* kt_disconnect ( kt_conn_session_t* sess )
 {
   zmq_close ( sess->_info->socket );
   zmq_ctx_destroy ( sess->_info->context );
   return sess->k_user_data;
-}
-
-kt_msg_t* kt_recv ( kt_conn_session_t* sess, int linger )
-{
-  void* buffer = malloc(100);
-  kt_msg* msg = kt_msg_new ();
-  int rc = zmq_recv ( sess->_info->socket, buffer, 100, 0 );
-  assert ( rc != -1 );
-
-  msg->payload = buffer;
-  msg->payload_size = rc;
-
-  return msg;
 }
 
  */
