a0a6a435865d43f0c6eef3998d7458fbbeb48487
Mathias Hablützel <habl@zhaw.ch>
2013-12-02 15:39:45 +0100

    Fixed indentation

--- include/KT_Client.hpp
+++ include/KT_Client.hpp
@@ -9,56 +9,57 @@ namespace KIARA {
 namespace Transport {
 
 /**
-  * class KT_Client
-  * 
-  */
+ * class KT_Client
+ *
+ */
 
 class KT_Client
 {
 private:
-  std::string _endpoint_identifier;
+	void* _socket;
+	std::string _endpoint_identifier;
 
 public:
 
-    //
-    // Constructors/Destructors
-    //  
+	//
+	// Constructors/Destructors
+	//
 
-  KT_Client ( );
-  virtual ~KT_Client ( );
+	KT_Client ( );
+	virtual ~KT_Client ( );
 
-  /**
-   * @param endpoint to be set
-   */
-  void set_endpoint ( std::string endpoint )
-  {
-    _endpoint_identifier = endpoint;
-  }
+	/**
+	 * @param endpoint to be set
+	 */
+	void set_endpoint ( std::string endpoint )
+	{
+		_endpoint_identifier = endpoint;
+	}
 
-  /**
-   * @return string
-   */
-  std::string get_endpoint ( )
-  {
-    return _endpoint_identifier;
-  }
+	/**
+	 * @return string
+	 */
+	std::string get_endpoint ( )
+	{
+		return _endpoint_identifier;
+	}
 
-  /**
-   * Set the value of _endpoint_identifier
-   * @param identifier the new value of _endpoint_identifier
-   */
-  void set_endpoint_identifier ( std::string identifier ) {
-      _endpoint_identifier = identifier;
-  }
+	/**
+	 * Set the value of _endpoint_identifier
+	 * @param identifier the new value of _endpoint_identifier
+	 */
+	void set_endpoint_identifier ( std::string identifier ) {
+		_endpoint_identifier = identifier;
+	}
 
-  /**
-   * Get the value of _endpoint_identifier
-   * @return the value of _endpoint_identifier
-   */
-  std::string get_endpoint_identifier ( ) {
-    return _endpoint_identifier;
-  }
-  
+	/**
+	 * Get the value of _endpoint_identifier
+	 * @return the value of _endpoint_identifier
+	 */
+	std::string get_endpoint_identifier ( ) {
+		return _endpoint_identifier;
+	}
+
 };
 } // end of Transport namespace
 } // end of KIARA namespace
