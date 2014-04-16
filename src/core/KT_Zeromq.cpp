icclab/kiara-transport

commit 8200c8f4a9c82e212354da31137c27a527f1e1bc

extract socket creation, deduplicate code
Signed-off-by: Mathias Hablützel <habl@zhaw.ch>

src/core/KT_Zeromq.cpp
@@ -32,27 +32,36 @@ KT_Zeromq::poller ( void* socket, std::string endpoint ) {
 	}
 }
 
+void* KT_Zeromq::create_socket (unsigned int socket_type)
+{
+    void* socket = nullptr;
+    int errcode = 0;
+    switch (socket_type)
+    {
+    case KT_STREAM:
+    case KT_WEBSERVER:
+        socket = zmq_socket ( _context, ZMQ_STREAM );
+        errcode = errno;
+        break;
+    case KT_REQUESTREPLY:
+        socket = zmq_socket ( _context, ZMQ_REQ);
+        errcode = errno;
+        break;
+    default:
+        break;
+    }
+    errno = errcode;
+    return socket;
+}
+
 int
 KT_Zeromq::connect ( KT_Session** ret ) {
-	void* socket = NULL;
+	void* socket = nullptr;
 	int errcode = 0;
 
-	switch (_configuration.get_application_type())
-	{
-	case KT_STREAM:
-	case KT_WEBSERVER:
-	    socket = zmq_socket ( _context, ZMQ_STREAM );
-	    errcode = errno;
-	    break;
-	case KT_REQUESTREPLY:
-	    socket = zmq_socket ( _context, ZMQ_REQ);
-	    errcode = errno;
-	    break;
-	default:
-	    break;
-	}
+	socket = create_socket(_configuration.get_application_type());
 
-	if (NULL == socket)
+	if (nullptr == socket)
 	{
 		errno = errcode;
 		return -1;
@@ -183,21 +192,12 @@ KT_Zeromq::register_callback ( std::function<void(KT_Msg&, KT_Session*, KT_Conne
  */
 int
 KT_Zeromq::bind ( ) {
-	void* socket = NULL;
+	void* socket = nullptr;
 	int errcode = 0;
 
-	if ( KT_STREAM == _configuration.get_application_type() )
-	{
-		socket = zmq_socket ( _context, ZMQ_STREAM );
-		errcode = errno;
-	}
-	else if ( KT_REQUESTREPLY == _configuration.get_application_type() )
-	{
-		socket = zmq_socket ( _context, ZMQ_REP);
-		errcode = errno;
-	}
+	socket = create_socket(_configuration.get_application_type());
 
-	if (NULL == socket)
+	if (nullptr == socket)
 	{
 		errno = errcode;
 		return -1;

src/core/KT_Zeromq.hpp
@@ -35,6 +35,7 @@ private:
 	std::thread* poller_thread;
 	bool interupted;
 	void poller ( void* socket, std::string endpoint );
+	void* create_socket(unsigned int socket_type);
 
 public:
 

commit f04c212bc3b2237371751ddeae6550b4234f035c

Fixing bug introduced by commit 8200c8f
Signed-off-by: Mathias Hablützel <habl@zhaw.ch>

src/core/KT_Zeromq.cpp
@@ -32,7 +32,7 @@ KT_Zeromq::poller ( void* socket, std::string endpoint ) {
 	}
 }
 
-void* KT_Zeromq::create_socket (unsigned int socket_type)
+void* KT_Zeromq::create_socket (unsigned int socket_type, bool listener)
 {
     void* socket = nullptr;
     int errcode = 0;
@@ -44,7 +44,12 @@ void* KT_Zeromq::create_socket (unsigned int socket_type)
         errcode = errno;
         break;
     case KT_REQUESTREPLY:
-        socket = zmq_socket ( _context, ZMQ_REQ);
+        if (listener)
+        {
+            socket = zmq_socket ( _context, ZMQ_REP);
+        } else {
+            socket = zmq_socket ( _context, ZMQ_REQ);
+        }
         errcode = errno;
         break;
     default:
@@ -59,7 +64,7 @@ KT_Zeromq::connect ( KT_Session** ret ) {
 	void* socket = nullptr;
 	int errcode = 0;
 
-	socket = create_socket(_configuration.get_application_type());
+	socket = create_socket(_configuration.get_application_type(), false);
 
 	if (nullptr == socket)
 	{
@@ -199,7 +204,7 @@ KT_Zeromq::bind ( ) {
 	void* socket = nullptr;
 	int errcode = 0;
 
-	socket = create_socket(_configuration.get_application_type());
+	socket = create_socket(_configuration.get_application_type(), true);
 
 	if (nullptr == socket)
 	{

src/core/KT_Zeromq.hpp
@@ -35,7 +35,7 @@ private:
 	std::thread* poller_thread;
 	bool interupted;
 	void poller ( void* socket, std::string endpoint );
-	void* create_socket(unsigned int socket_type);
+	void* create_socket(unsigned int socket_type, bool listener);
 
 public:
 